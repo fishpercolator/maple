bb7e26348a02385cec22789b85c1409841fbefd6
Rich Daley <rich@fishpercolator.co.uk>
2018-01-19 15:51:51 +0000

    Reword about page

--- pages/about.vue
+++ pages/about.vue
@@ -3,19 +3,20 @@
     <div class="leftright">
       <div id="description" class="left">
         <div>
-          <h2>Maple <small><strong>- Map</strong>ping <strong>Le</strong>eds</small></h2>
-          <p>Maple is a tool for quickly generating maps representing data in Leeds.</p>
-          <p>It can generate two types of maps - ward maps and postcode maps.</p>
+          <h2><img src="~assets/maple.svg" /> Maple <small><strong>- Map</strong>ping <strong>Le</strong>eds</small></h2>
+          <p>Maple is a tool for generating maps of geographic data about Leeds; for example, if you want to visualise your geographic data in a report or presentation. It is aimed at people who already have the data, but are having difficulty presenting it in a meaningful way.</p>
+          <p>It can generate two types of maps - ward maps and postcode maps, which are explained below.</p>
+          <p>For the best experience, run Maple in Google Chrome on a computer rather than a mobile device. This is because it uses some fancy new web tech that might not be available on all platforms.</p>
           <p>Maple was commissioned by <a href="https://timetoshineleeds.org/">Time to Shine</a> and developed by <a href="https://www.fishpercolator.co.uk/">Fish Percolator</a> using <a href="https://vuejs.org/">Vue.js</a>.</p>
           <h3>Ward maps</h3>
           <p>A <router-link to="/">ward map</router-link> allows you to visualise any numeric dataset with values for each ward in Leeds. Examples of such datasets include things like spending, vote shares, number of people...</p>
           <p>Simply choose a colour and fill your values in. The brightest intensity will be given to the ward with the largest value in your dataset, and then other wards will be given an intensity proportional to that value.</p>
-          <img src="~assets/wardmaps.png" />
+          <img class="fig" src="~assets/wardmaps.png" />
           <p>The map will update as you change the values, and it auto-saves so you can close the tab and come back to it whenever you like.</p>
           <p>When you're ready, you can click the 'save map as image' button to download the map. This uses a screenshot, so make sure the map is positioned as you'd like it before you press the button.</p>
           <h3>Postcode maps</h3>
           <p>A <router-link to="/postcodes">postcode map</router-link> is a heatmap that visualises a set of postcodes, showing where they are most clustered. An example (in the image below) might be the locations of pedestrian crossings.</p>
-          <img src="~assets/postcodemap.png" />
+          <img class="fig" src="~assets/postcodemap.png" />
           <p>Enter your postcodes either one per line, or separated by commas. There are also two sliders you can use to control the map: <em>Radius</em> is the size of the circle given to each postcode on the map, and <em>Max intensity</em> can put a limit on the maximum intensity of the map, in order that very strong don't nullify the rest of the map. You can play with these sliders and see how they affect your map.</p>
           <p>When you're ready, click the 'save map as image' button to download the map. This uses a screenshot, so make sure the map is positioned as you'd like it before you press the button.</p>
           <h3>Datasets used</h3>
@@ -46,6 +47,7 @@ export default {
     flex-direction: column;
     padding: 1em;
     align-items: center;
+    height: auto;
     h2 {
       small {
         font-size: 0.5em;
@@ -56,7 +58,7 @@ export default {
         }
       }
     }
-    img {
+    img.fig {
       border: 2px solid black;
       display: block;
       margin: 2em auto;
